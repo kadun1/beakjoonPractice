368e9ea1c64db6f219f160c2d449f75ab8214232
kadun1 <kadunjj@gmail.com>
2022-03-03 21:53:40 +0900

    백준코딩테스트 입출력3회차

---
+++ src/P03sysout.java
@@ -0,0 +1,8 @@
+public class P03sysout {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
