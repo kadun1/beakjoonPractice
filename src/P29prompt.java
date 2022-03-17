e6d60274d0767c945e867391e533638b04f14573
kadun1 <kadunjj@gmail.com>
2022-03-17 22:35:50 +0900

    백준코딩테스트 프롬프트 완

--- src/P29prompt.java
+++ src/P29prompt.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class P29prompt {
     public static void main(String[] args) throws IOException {
@@ -8,13 +6,34 @@ public class P29prompt {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         String[] strArr = new String[n];
-//        List<String> strList = new ArrayList<String>();
+
         for (int i = 0; i < strArr.length; i++) {
             strArr[i] = br.readLine();
         }
 
-        for (String s : strArr) {
-            System.out.println("s = " + s);
+        String[][] strings = new String[n][strArr.length];
+        for (int i = 0; i < n; i++) {
+            strings[i] = strArr[i].split("");
         }
+
+        String[] resultArr = strings[0];
+
+        for (int i = 1; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length; j++) {
+                if(!strings[i-1][j].equals(strings[i][j])){
+                    resultArr[j] = "?";
+                    continue;
+                } else {
+                    if(!resultArr[j].equals("?")){
+                        resultArr[j] = strings[i][j];
+                    }
+                }
+            }
+        }
+        String result = "";
+        for (String s: resultArr) {
+            result += s;
+        }
+        System.out.println(result);
     }
 }
